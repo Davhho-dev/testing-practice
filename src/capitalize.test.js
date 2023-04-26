22781ad0ec5938fbadf8fb80a8dd16e2af99e89c
Davhho-dev <Davhho@utexas.edu>
2023-04-25 20:58:11 -0500

    Add test to check if argument is passed and expect null

--- src/capitalize.test.js
+++ src/capitalize.test.js
@@ -3,3 +3,7 @@ const capitalize = require("./capitalize");
 test("lowercase string 'hello' should return first character as 'H'", () => {
     expect(capitalize("hello")).toBe("H");
 });
+
+test("Return null if no argument is passed", () => {
+    expect(capitalize()).toBeNull();
+});
