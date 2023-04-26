d72b07bebc8e526fc96488c31189f2c2ef9d2afe
Davhho-dev <Davhho@utexas.edu>
2023-04-25 21:32:33 -0500

    Rewrote code to return entire string instead of only the first char capitalized

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,8 +1,9 @@
 function capitalize(str) {
     if(str === undefined) return null;
     if(typeof str !== "string") throw typeof str;
-    const capStr = str.toUpperCase();
-    return capStr.charAt(0);
+    let capChar = str.charAt(0).toUpperCase();
+    let remainderStr = str.slice(1);
+    return capChar.concat(remainderStr);
 }
 
 
