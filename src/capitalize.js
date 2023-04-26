2a04e5f5f5d7355819066de2fedcca85683ce1cf
Davhho-dev <Davhho@utexas.edu>
2023-04-25 21:20:13 -0500

    Check if argument is a string

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,8 +1,9 @@
 function capitalize(str) {
     if(str === undefined) return null;
-    console.log(str);
+    if(typeof str !== "string") throw typeof str;
     const capStr = str.toUpperCase();
     return capStr.charAt(0);
 }
+
 
 module.exports = capitalize;
