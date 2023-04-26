27dab6f2ffab268625d9d9cd1fbf85151f17853d
Davhho-dev <Davhho@utexas.edu>
2023-04-25 21:48:09 -0500

    Make str argument start off as lowercase for cases where string is all capitalized

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,8 +1,9 @@
 function capitalize(str) {
     if(str === undefined) return null;
     if(typeof str !== "string") throw typeof str;
-    let capChar = str.charAt(0).toUpperCase();
-    let remainderStr = str.slice(1);
+    const lowerCase = str.toLowerCase()
+    let capChar = lowerCase.charAt(0).toUpperCase();
+    let remainderStr = lowerCase.slice(1);
     return capChar.concat(remainderStr);
 }
 
