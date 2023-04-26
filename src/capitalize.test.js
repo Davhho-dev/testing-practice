ad211cb191ebb06d1800161686d190509b1f96ec
Davhho-dev <Davhho@utexas.edu>
2023-04-25 21:20:53 -0500

    Add test to check if argument is a string return throw exception

--- src/capitalize.test.js
+++ src/capitalize.test.js
@@ -7,3 +7,9 @@ test("lowercase string 'hello' should return first character as 'H'", () => {
 test("Return null if no argument is passed", () => {
     expect(capitalize()).toBeNull();
 });
+
+test("Throw error if a string argument is not passed", () => {
+    expect(() => {
+        capitalize(1);
+    }).toThrow();
+});
