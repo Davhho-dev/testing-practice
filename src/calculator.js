cf44f0270c0556b9a4db296cd0fa6f9b89439793
Davhho-dev <Davhho@utexas.edu>
2023-04-30 16:18:36 -0500

    Add code to check for non number arguments

--- src/calculator.js
+++ src/calculator.js
@@ -1,9 +1,18 @@
 const calculator = (x, y) => {
     let sum = null;
+    
+    const notValidArg = (x, y) => {
+        if(typeof x !== "number" || typeof y !== "number") return true;
+        else return false;
+    }
+    
     const add = (x, y) => {
-        sum = x + y;
-        console.log(sum);
-        return sum;
+        if(notValidArg(x, y)) return "Arguments contain a non number";
+        else {
+            sum = x + y;
+            console.log(sum);
+            return sum;
+        }
     }
     return {add};
 }
