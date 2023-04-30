4bca991f969c5a966fe35bdb19ec5c899337d842
Davhho-dev <Davhho@utexas.edu>
2023-04-30 16:19:02 -0500

    Add test for non number arguments

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -5,3 +5,7 @@ const calc = calculator();
 test("Adding two numbers", () => {
     expect(calc.add(10, 20)).toBe(30);
 });
+
+test("Check for non number arguments", () => {
+    expect(calc.add(10, "x")).toBe("Arguments contain a non number");
+});
