4652b0e554687978776b93363683b0095a3d619d
Davhho-dev <Davhho@utexas.edu>
2023-04-30 15:28:58 -0500

    Add check and test for no arguments passed

--- src/reversestring.js
+++ src/reversestring.js
@@ -1,9 +1,9 @@
 function reverseString(str) {
+    if(str === undefined) return null;
     let result = "";
     for(let i = str.length - 1; i >= 0; i--) {
         result += str.charAt(i);
     }
-    // console.log(result);
     return result;
 }
 
--- src/reversestring.test.js
+++ src/reversestring.test.js
@@ -3,3 +3,7 @@ const reverseString = require("./reversestring");
 test("Pass 'Hello' returns 'olleh'", () => {
     expect(reverseString("Hello")).toBe("olleH");
 });
+
+test("Passing no arguments should return null", () => {
+    expect(reverseString()).toBeNull();
+});
