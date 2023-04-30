63e62ddca2b9fe9a8b2ddf69961364d564867636
Davhho-dev <Davhho@utexas.edu>
2023-04-30 15:34:19 -0500

    Create check and test for nonstring arguments

--- src/reversestring.test.js
+++ src/reversestring.test.js
@@ -7,3 +7,9 @@ test("Pass 'Hello' returns 'olleh'", () => {
 test("Passing no arguments should return null", () => {
     expect(reverseString()).toBeNull();
 });
+
+test("Passing nonstring throws error", () => {
+    expect(() => {
+        reverseString(123);
+    }).toThrow();
+});
