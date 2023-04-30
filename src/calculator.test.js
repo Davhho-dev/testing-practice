83929fc89c9b6b7cd289ebb516216b97a7e28ea2
Davhho-dev <Davhho@utexas.edu>
2023-04-30 16:25:02 -0500

    Add test for subtracting two numbers and checking for non number arguments

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -8,4 +8,10 @@ test("Adding two numbers", () => {
 
 test("Check for non number arguments", () => {
     expect(calc.add(10, "x")).toBe("Arguments contain a non number");
+    expect(calc.subtract(10, "5")).toBe("Arguments contain a non number");
 });
+
+test("Subtracting two numbers", () => {
+    expect(calc.subtract(20, 10)).toBe(10);
+});
+
