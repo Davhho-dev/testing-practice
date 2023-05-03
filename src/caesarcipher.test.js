5f687649a212b9ba1af338e3961e2e849df01176
Davhho-dev <Davhho@utexas.edu>
2023-05-02 23:13:51 -0500

    Create test to test shifting one word string argument

--- src/caesarcipher.test.js
+++ src/caesarcipher.test.js
@@ -1,5 +1,9 @@
 const caesarCipher = require("./caesarcipher");
 
-test("Encryption function shifting correctly", () => {
-    expect(caesarCipher("abcdefghijklmnopqrstuvwxyz", 5)).toBe("fghijklmnopqrstuvwxyzabcde");
+// test("Encryption function shifting correctly", () => {
+//     expect(caesarCipher("abcdefghijklmnopqrstuvwxyz", 5)).toBe("fghijklmnopqrstuvwxyzabcde");
+// });
+
+test("Shifting string with one word", () => {
+    expect(caesarCipher("hello", 5)).toBe("mjqqt");
 });
