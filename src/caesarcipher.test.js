0c0eb47be4f6a0d2d3f2acde89752313bbdf5fb6
Davhho-dev <Davhho@utexas.edu>
2023-05-02 23:19:09 -0500

    Add test for string with two words and a space

--- src/caesarcipher.test.js
+++ src/caesarcipher.test.js
@@ -7,3 +7,7 @@ const caesarCipher = require("./caesarcipher");
 test("Shifting string with one word", () => {
     expect(caesarCipher("hello", 5)).toBe("mjqqt");
 });
+
+test("shifting string with two word and accounting for space", () => {
+    expect(caesarCipher("hello world", 2)).toBe("jgnnq yqtnf");
+});
