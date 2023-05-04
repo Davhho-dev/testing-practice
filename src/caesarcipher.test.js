c1adb80f255491ea2b58c27a42c6b806b8b04ac3
Davhho-dev <Davhho@utexas.edu>
2023-05-04 18:51:43 -0500

    Add another basic multiple word with space test for fun

--- src/caesarcipher.test.js
+++ src/caesarcipher.test.js
@@ -8,8 +8,9 @@ test("Shifting string with one word", () => {
     expect(caesarCipher("hello", 5)).toBe("mjqqt");
 });
 
-test("shifting string with two word and accounting for space", () => {
+test("shifting string with multiple words and accounting for space", () => {
     expect(caesarCipher("hello world", 2)).toBe("jgnnq yqtnf");
+    expect(caesarCipher("itsa me mario", 15)).toBe("xihp bt bpgxd");
 });
 
 test("shifting all caps string argument returns correct lowercase shifted result", () => {
